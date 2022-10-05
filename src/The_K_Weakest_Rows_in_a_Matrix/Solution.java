5c3a89ec5b169c18614ba87f750b7b066631d6b0
MykolaMurza <murza.na00@gmail.com>
2022-10-05 16:27:39 +0300

    Add 1 task

---
+++ src/The_K_Weakest_Rows_in_a_Matrix/Solution.java
@@ -0,0 +1,49 @@
+package The_K_Weakest_Rows_in_a_Matrix;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(kWeakestRows(new int[][]{{1, 1, 0, 0, 0},
+                {1, 1, 1, 1, 0}, {1, 0, 0, 0, 0}, {1, 1, 0, 0, 0}, {1, 1, 1, 1, 1}}, 3)));
+        System.out.println(Arrays.toString(kWeakestRows(new int[][]{{1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+                {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}, 1)));
+    }
+
+    public static int[] kWeakestRows(int[][] mat, int k) {
+        int[] strength = new int[mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            int lvlSum = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                lvlSum += mat[i][j];
+            }
+            strength[i] = lvlSum * 10000 + i;
+        }
+
+        for (int i = 0; i < strength.length - 1; i++) {
+            for (int j = i + 1; j < strength.length; j++) {
+                if (strength[i] > (strength[j])) {
+                    int temp = strength[i];
+                    strength[i] = strength[j];
+                    strength[j] = temp;
+                }
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < res.length; i++) {
+            String str = String.valueOf(strength[i]);
+            int lastIndexOf = str.lastIndexOf("00");
+            res[i] = Integer.parseInt(str.substring(lastIndexOf + 1));
+        }
+
+        return res;
+    }
+}
