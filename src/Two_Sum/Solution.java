e535cfe5f27e0296c2739e39e92ecf35f8bbf8c4
MykolaMurza <murza.na00@gmail.com>
2022-10-09 23:29:28 +0300

    Add 1 task

--- src/Two_Sum/Solution.java
+++ src/Two_Sum/Solution.java
@@ -4,19 +4,17 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(Arrays.equals(twoSum(new int[]{2, 7, 11, 15}, 9), new int[]{0, 1}));
-        System.out.println(Arrays.equals(twoSum(new int[]{3, 2, 4}, 6), new int[]{1, 2}));
-        System.out.println(Arrays.equals(twoSum(new int[]{3, 3}, 6), new int[]{0, 1}));
-        System.out.println(Arrays.equals(twoSum(new int[]{2, 5, 5, 11}, 10), new int[]{1, 2}));
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 5, 5, 11}, 10)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        if (nums.length > 2) {
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[i] + nums[j] == target) {
-                        return new int[]{i, j};
-                    }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] + nums[j - i] == target) {
+                    return new int[]{j, j - i};
                 }
             }
         }
