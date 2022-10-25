079a06d1faf0c65044f204d8c463a3004ef2428d
MykolaMurza <murza.na00@gmail.com>
2022-10-25 17:06:46 +0300

    Add 1 task

---
+++ src/Check_If_Two_String_Arrays_are_Equivalent/Solution.java
@@ -0,0 +1,17 @@
+package Check_If_Two_String_Arrays_are_Equivalent;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (String s : word1) sb1.append(s);
+        for (String s : word2) sb2.append(s);
+
+        return sb1.toString().equals(sb2.toString());
+    }
+}
