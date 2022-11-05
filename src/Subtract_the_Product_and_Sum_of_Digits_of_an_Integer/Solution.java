92207a67846efb2ce48baa8b340cc8a8f7ee0c17
MykolaMurza <murza.na00@gmail.com>
2022-11-06 01:56:28 +0200

    add todo

---
+++ src/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer/Solution.java
@@ -0,0 +1,12 @@
+package Subtract_the_Product_and_Sum_of_Digits_of_an_Integer;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(subtractProductAndSum(234));
+        System.out.println(subtractProductAndSum(4421));
+    }
+
+    public static int subtractProductAndSum(int n) {
+        //todo
+    }
+}
