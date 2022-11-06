9259255688d5bd5f52c97328ac8b0e847ce5bfc8
MykolaMurza <murza.na00@gmail.com>
2022-11-06 02:07:51 +0200

    Add 1 task

--- src/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer/Solution.java
+++ src/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer/Solution.java
@@ -4,9 +4,20 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(subtractProductAndSum(234));
         System.out.println(subtractProductAndSum(4421));
+        System.out.println(subtractProductAndSum(705));
+        System.out.println(subtractProductAndSum(690));
     }
 
     public static int subtractProductAndSum(int n) {
-        //todo
+        int prevMul = 1, prevSum = 0;
+
+        while (n != 0) {
+            int val = n % 10;
+            prevSum += val;
+            prevMul *= val;
+            n /= 10;
+        }
+
+        return prevMul - prevSum;
     }
 }
