124babcf19defeb577b5a149321f4ee794299aa7
MykolaMurza <murza.na00@gmail.com>
2024-09-02 23:08:36 +0200

    Rewrite 'Average_Salary_Excluding_the_Minimum_and_Maximum_Salary'

--- src/Average_Salary_Excluding_the_Minimum_and_Maximum_Salary/Solution.java
+++ src/Average_Salary_Excluding_the_Minimum_and_Maximum_Salary/Solution.java
@@ -3,15 +3,23 @@ package Average_Salary_Excluding_the_Minimum_and_Maximum_Salary;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(average(new int[]{4000, 3000, 1000, 2000}));
+        System.out.println(average(new int[]{1000, 2000, 3000}));
     }
 
     public static double average(int[] salary) {
-        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, sum = 0;
+        int sum = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
         for (int i = 0; i < salary.length; i++) {
-            sum += salary[i];
-            max = Math.max(max, salary[i]);
-            min = Math.min(min, salary[i]);
+            int currentEmployeeSalary = salary[i];
+            if (currentEmployeeSalary > max) {
+                max = currentEmployeeSalary;
+            }
+
+            if (currentEmployeeSalary < min) {
+                min = currentEmployeeSalary;
+            }
+
+            sum += currentEmployeeSalary;
         }
 
         return (double) (sum - max - min) / (salary.length - 2);
