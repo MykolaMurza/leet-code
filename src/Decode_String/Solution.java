ee0734371055b79d0bf88a6855611e7ccf31e9ef
MykolaMurza <murza.na00@gmail.com>
2025-01-28 20:19:47 +0200

    Add Decode_String

--- src/Decode_String/Solution.java
+++ src/Decode_String/Solution.java
@@ -1,11 +1,12 @@
 package Decode_String;
 
-import java.util.Arrays;
-
 class Solution {
     public static void main(String[] args) {
-        System.out.println(decodeString("3[a]2[bc]"));
-        System.out.println(decodeString("abc3[a]2[bc]abc"));
+        System.out.println(decodeString("3[a]2[bc]")); // aaa-bcbc
+        System.out.println(decodeString("abc3[a]2[bc]abc")); // abc-aaa-bcbc-abc
+        System.out.println(decodeString("3[a2[b]a]2[bc]")); // abba-abba-abba-bcbc
+        System.out.println(decodeString("3[a2[b]a]2[bc]a2[b]a")); // abba-abba-abba-bcbc-a-bb-a
+        System.out.println(decodeString("3[3[3[a]]]")); // aaa-aaa-aaa-aaa-aaa-aaa-aaa-aaa-aaa
     }
 
     public static String decodeString(String s) {
@@ -13,18 +14,26 @@ class Solution {
         StringBuilder[] parts = new StringBuilder[input.length];
         int writer = 0;
 
+        int openings = 0;
         for (int i = 0; i < input.length; i++) {
+//            System.out.println(input[i] + " : ops " + openings);
             if (input[i] >= '0' && input[i] <= '9') {
-                if (i > 0 && input[i - 1] >= 'a' && input[i - 1] <= 'z') {
+                if (i > 0 && input[i - 1] >= 'a' && input[i - 1] <= 'z' && openings == 0) {
                     writer++;
                 }
                 parts[writer] = checkAndAppend(parts[writer], input[i]);
             } else if (input[i] == '[') {
-                writer++;
+                if (openings == 0) {
+                    writer++;
+                }
+                openings++;
                 parts[writer] = checkAndAppend(parts[writer], '[');
             } else if (input[i] == ']') {
+                openings--;
                 parts[writer] = checkAndAppend(parts[writer], ']');
-                writer++;
+                if (openings == 0) {
+                    writer++;
+                }
             } else {
                 parts[writer] = checkAndAppend(parts[writer], input[i]);
             }
@@ -34,11 +43,14 @@ class Solution {
             writer--;
         }
 
-        System.out.println(Arrays.toString(parts));
+//        System.out.println(Arrays.toString(parts));
 
-        StringBuilder result = concatResult(writer, parts);
+        String result = concatResult(writer, parts).toString();
+        if (result.contains("[")) {
+            return decodeString(result);
+        }
 
-        return result.toString();
+        return result;
     }
 
     private static StringBuilder checkAndAppend(StringBuilder sb, char element) {
@@ -55,7 +67,7 @@ class Solution {
 
         for (int j = 0; j <= writer; j++) {
             String element = parts[j].toString();
-            if (element.charAt(0) >= '0' && element.charAt(0) <= '9' ) {
+            if (element.charAt(0) >= '0' && element.charAt(0) <= '9') {
                 iterationsCount = Integer.parseInt(parts[j].toString());
             } else if (element.charAt(0) == '[') {
                 element = element.substring(1, element.length() - 1);
